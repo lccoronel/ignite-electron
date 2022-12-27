2a89534f7d45650fd9bc1cfad7e86032dc6c8d23
Lucas Coronel <lucas.coronel@platformbuilders.io>
2022-12-27 18:45:14 -0300

    feat: document page

--- rotion/src/renderer/src/Routes.tsx
+++ rotion/src/renderer/src/Routes.tsx
@@ -1,6 +1,7 @@
 import { Router, Route } from 'electron-router-dom'
 
 import { Blank } from './pages/blank'
+import { Document } from './pages/Document'
 import { Default } from './pages/layouts/default'
 
 export function Routes() {
@@ -9,6 +10,7 @@ export function Routes() {
       main={
         <Route path="/" element={<Default />}>
           <Route path="/" element={<Blank />} />
+          <Route path="/document" element={<Document />} />
         </Route>
       }
     />
